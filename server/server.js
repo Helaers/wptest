b2be653563f0130ce66595414332454f95fe2753
Sfen Helaers <sfenhelaers@gmail.com>
2016-09-08 12:36:18 +0200

    edit server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
 
-//app.use('/', express.static( path.resolve(__dirname, '../dist/index.html')));
+app.use('/', express.static( path.resolve(__dirname, '../dist/index.html')));
 
 app.get('/', function (req, res) {
    res.send('Hello World');
 })
 
-app.listen(3000);
+app.listen(proccess.evn.PORT || 3000);
 

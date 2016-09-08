2188ff60aa9d4d49b6887cde91df2ca03afcc11c
Sfen Helaers <sfenhelaers@gmail.com>
2016-09-08 10:49:32 +0200

    added basic server

---
+++ server/server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+var port = 3000;
+
+app.use('/', express.static( path.resolve(__dirname, '../dist')));
+app.listen(port, function () {
+ console.log('Example app listening on port ' + port + ' !');
+});
+

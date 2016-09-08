cb44683e24908b8228e05f33af53efda830cc8a4
Sfen Helaers <sfenhelaers@gmail.com>
2016-09-08 11:10:17 +0200

    changed server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var path = require('path');
-
-var app = express();
-var port = 3000;
+var express = require('express')
+var app = express()
 
 app.use('/', express.static( path.resolve(__dirname, '../dist/index.html')));
-app.listen(port, function () {
- console.log('Example app listening on port ' + port + ' !');
-});
+
+app.get('/', function (req, res) {
+   res.send('Hello World')
+})
+
+app.listen(3000);
 
